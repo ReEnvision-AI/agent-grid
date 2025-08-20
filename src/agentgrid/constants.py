6fe24573598f405b6c438a96afda45d5571a05a2
Brad Jones <pgawestjones@gmail.com>
2025-08-20 10:54:15 -0700

    Using new p2p id for bootstrap

--- src/agentgrid/constants.py
+++ src/agentgrid/constants.py
@@ -4,7 +4,7 @@ from hivemind.utils import get_logger
 
 logger = get_logger(__name__)
 
-PUBLIC_INITIAL_PEERS = ['/dns4/sociallyshaped.net/tcp/8788/p2p/QmTUpY86VSyvwvBN8oc9W3JztLaxyabT6b17gnXxdfx5HL']
+PUBLIC_INITIAL_PEERS = ['/dns4/sociallyshaped.net/tcp/8788/p2p/QmSt3bPSboHuBNfgB3tPrjGnW1D3xFRPyvrmi2x7TiZ3qR']
 
 # The reachability API is currently used only when connecting to the public swarm
 REACHABILITY_API_URL = "https://sociallyshaped.net/health"
